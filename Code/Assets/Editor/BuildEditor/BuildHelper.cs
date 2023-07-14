170ed96f95f317e1b53012aad6439c69cd8390aa
Wynn <wynnforthework@gmail.com>
2023-07-14 16:06:15 +0800

    Update BuildHelper.cs

--- Code/Assets/Editor/BuildEditor/BuildHelper.cs
+++ Code/Assets/Editor/BuildEditor/BuildHelper.cs
@@ -112,7 +112,8 @@ namespace ET
         {
             PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android,ScriptingImplementation.Mono2x);
             PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARMv7;
-            
+            PlayerSettings.applicationIdentifier = "com.pocket.topbrowser";
+            PlayerSettings.Android.useCustomKeystore = false;
             var opa = BuildOptions.CompressWithLz4HC | BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler | BuildOptions.EnableDeepProfilingSupport;
 
             BuildHelper.Build(BuildType.Release,PlatformType.Android, BuildAssetBundleOptions.ForceRebuildAssetBundle | BuildAssetBundleOptions.ChunkBasedCompression, opa, true, true, true);
@@ -123,6 +124,8 @@ namespace ET
         {
             PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android,ScriptingImplementation.IL2CPP);
             PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARM64;
+            PlayerSettings.applicationIdentifier = "com.fulljoblegend.android";
+            PlayerSettings.Android.useCustomKeystore = true;
 
             var opa = BuildOptions.CompressWithLz4HC;
 
@@ -180,9 +183,12 @@ namespace ET
 
                 if (isBuildExe)
                 {
-                    PlayerSettings.Android.bundleVersionCode++;
-                    PlayerSettings.bundleVersion = string.Join(".", PlayerSettings.Android.bundleVersionCode.ToString().PadLeft(3,'0').ToCharArray());
-
+                    if (buildType == BuildType.Release)
+                    {
+                        PlayerSettings.Android.bundleVersionCode++;
+                        PlayerSettings.bundleVersion = string.Join(".", PlayerSettings.Android.bundleVersionCode.ToString().PadLeft(3,'0').ToCharArray());
+                    }
+                    
                     AssetDatabase.Refresh();
                     string[] levels = {
                         "Assets/Scenes/Init.unity",
