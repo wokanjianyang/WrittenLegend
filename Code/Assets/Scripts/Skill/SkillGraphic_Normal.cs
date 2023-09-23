18fd80c6a840d00efd3e5792f0f6da0912ef0b0d
wokanjianyang <569284543@qq.com>
2023-09-23 11:09:35 +0800

    temp

--- Code/Assets/Scripts/Skill/SkillGraphic_Normal.cs
+++ Code/Assets/Scripts/Skill/SkillGraphic_Normal.cs
@@ -7,7 +7,7 @@ namespace Game
 {
     public class SkillGraphic_Normal : SkillGraphic
     {
-        const float speed = 0.3f;
+        const float speed = 0.2f;
 
         public SkillGraphic_Normal(APlayer player, SkillPanel skill) : base(player, skill)
         {
@@ -22,7 +22,7 @@ namespace Game
         {
             yield return new WaitForSeconds(speed);
             var distance = cell - this.SelfPlayer.Cell;
-            Vector3 offset = new Vector3(distance.x * 0.5f, distance.y * 0.5f) * GameProcessor.Inst.MapData.CellSize.x;
+            Vector3 offset = new Vector3(distance.x * 0.2f, distance.y * 0.2f) * GameProcessor.Inst.MapData.CellSize.x;
             var targetPos = GameProcessor.Inst.MapData.GetWorldPosition(this.SelfPlayer.Cell);
             this.SelfPlayer.Transform.DOLocalMove(targetPos + offset, speed);
 
