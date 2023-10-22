66f8482613245e91aba48dfd8030506f14465883
wokanjianyang <569284543@qq.com>
2023-10-22 21:53:49 +0800

    temp154

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -4,7 +4,7 @@ namespace Game
 {
 	public class ConfigHelper
 	{
-		public const int Version = 153;
+		public const int Version = 154;
 
 		public const long Max_Level = 20000; //最大人物等级和强化等级
 
@@ -17,13 +17,13 @@ namespace Game
 		public static int[] RuneRate = new int[] { 1, 3, 8, 18, 39, 85, 185 }; //词条产生概率
 		public static int[] RuneRate1 = new int[] { 1, 3, 6, 9, 15, 24, 39 }; //橙色和专属产生概率
 
-		public const long PackTime = 1697528842; //打包时间，防止作弊
+		public const long PackTime = 1697959930; //打包时间，防止作弊
 
-		public const long PackEndTime = 1698565642; //超过此时间,游戏不能使用，需要更新
+		public const long PackEndTime = 1698996730; //超过此时间,游戏不能使用，需要更新
 
 		public const long MaxOfflineTime = 3600 * 12;  //最长离线时间
 
-		public const int MaxBagCount = 150;  // 包裹数量
+		public const int MaxBagCount = 210;  // 包裹数量
 
 		public const int CopyTicketCd = 900; //15分钟
 
