f5a7701d8f6681454b732d098a3080a42da73405
wokanjianyang <569284543@qq.com>
2024-01-02 23:27:33 +0800

    temp

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -4,7 +4,7 @@ namespace Game
 {
     public class ConfigHelper
     {
-        public const int Version = 166;
+        public const int Version = 167;
 
         public const long Max_Level = 40000; //最大人物等级和强化等级
 
