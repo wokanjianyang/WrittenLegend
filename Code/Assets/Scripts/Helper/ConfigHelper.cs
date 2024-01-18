0046251e6f90825f1a5f58ccb3549af1c32a63df
admin <569284543@qq.com>
2024-01-18 19:59:02 +0800

    temp

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -4,7 +4,7 @@ namespace Game
 {
     public class ConfigHelper
     {
-        public const int Version = 169;
+        public const int Version = 170;
 
         public const long Max_Level = 50000; //最大人物等级和强化等级
 
@@ -18,9 +18,9 @@ namespace Game
         public static int[] RuneRate1 = new int[] { 1, 3, 6, 9, 15, 24, 33, 42 }; //橙色装备产生概率
         public static int[] RuneRate2 = new int[] { 1, 3, 6, 9, 15, 24, 39 }; //专属产生概率
 
-        public const long PackTime = 1704527569; //打包时间，防止作弊
+        public const long PackTime = 1705565059; //打包时间，防止作弊
 
-        public const long PackEndTime = 1705564369; //超过此时间,游戏不能使用，需要更新
+        public const long PackEndTime = 1706601859; //超过此时间,游戏不能使用，需要更新
 
         public const long MaxOfflineTime = 3600 * 24;  //最长离线时间
 
