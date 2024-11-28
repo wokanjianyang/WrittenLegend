15108703b1f78baccb3e7e693aeb3f1afc0a6c44
admin <569284543@qq.com>
2024-11-28 16:12:57 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -12,7 +12,7 @@ namespace Game
 
         public const int Channel_Tap = 1;
 
-        public const int Version = 305;
+        public const int Version = 306;
 
         public const long PackTime = 1732527499; //打包时间，防止作弊
 
