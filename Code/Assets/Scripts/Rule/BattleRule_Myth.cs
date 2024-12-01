38d879d0661e0b4ae7ccec6b060c9036bb41bee2
wokanjianyang <569284543@qq.com>
2024-12-02 00:05:08 +0800

    t

--- Code/Assets/Scripts/Rule/BattleRule_Myth.cs
+++ Code/Assets/Scripts/Rule/BattleRule_Myth.cs
@@ -74,6 +74,7 @@ public class BattleRule_Myth : ABattleRule
             this.Start = false;
 
             GameProcessor.Inst.EventCenter.Raise(new BattleMsgEvent() { Type = RuleType.Myth, Message = "ÃÙ’ΩÕ®πÿ£°" });
+            GameProcessor.Inst.User.MythData.SetOver(this.MapId);
             BuildReward(MapId);
 
             GameProcessor.Inst.CloseBattle(RuleType.Myth, 13);
