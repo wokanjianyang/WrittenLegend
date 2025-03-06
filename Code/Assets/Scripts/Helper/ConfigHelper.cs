726305f656235f75238834e2383b613872372cfe
admin <569284543@qq.com>
2025-03-06 17:29:17 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -12,7 +12,7 @@ namespace Game
 
         public const int Channel_Tap = 1;
 
-        public const int Version = 359;
+        public const int Version = 360;
 
         public const long PackTime = 1741154473; //打包时间，防止作弊
 
