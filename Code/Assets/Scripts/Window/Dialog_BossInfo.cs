7482f23f391fe3727937ec32685550c473dea331
wokanjianyang <569284543@qq.com>
2025-03-17 22:54:58 +0800

    t

--- Code/Assets/Scripts/Window/Dialog_BossInfo.cs
+++ Code/Assets/Scripts/Window/Dialog_BossInfo.cs
@@ -138,7 +138,7 @@ public class Dialog_BossInfo : MonoBehaviour, IBattleLife
 
         if (this.SelectLayer < 0)
         {
-            this.SelectLayer = Math.Min(this.MaxLayer, 3);
+            this.SelectLayer = Math.Min(this.MaxLayer, 4);
             tgLevelList[SelectLayer].isOn = true;
         }
 
