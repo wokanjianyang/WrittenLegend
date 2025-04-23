a70a007b7a60884731281a991d839f585b4477a6
admin <569284543@qq.com>
2025-04-23 16:40:04 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -14,9 +14,9 @@ namespace Game
 
         public const int Version = 376;
 
-        public const long PackTime = 1744462106; //打包时间，防止作弊
+        public const long PackTime = 1745397438; //打包时间，防止作弊
 
-        public const long PackEndTime = 1746190106; //超过此时间,游戏不能使用，需要更新
+        public const long PackEndTime = 1747125438; //超过此时间,游戏不能使用，需要更新
 
         public const long Max_Level = 120000; //最大人物等级和强化等级
 
