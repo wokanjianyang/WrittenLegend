de5f97b875b954216b761748666e81490abff2a8
wokanjianyang <569284543@qq.com>
2025-04-25 21:47:06 +0800

    t

--- Code/Assets/Scripts/Generate/EquipConfigCategory.cs
+++ Code/Assets/Scripts/Generate/EquipConfigCategory.cs
@@ -52,7 +52,7 @@ namespace Game
                 }
             }
 
-            Equip equip = new Equip(configId, 12010, 12110, quality);
+            Equip equip = new Equip(configId, runeId, suitId, quality);
             if (seed < 0)
             {
                 seed = AppHelper.InitSeed();
