ed85c8c6679d3aaf79b900f6c361296ca20091fb
wokanjianyang <569284543@qq.com>
2025-05-07 17:04:23 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -98,7 +98,7 @@ namespace Game
 
         public const int Mine_Time = 60;
 
-        public const int Infinit_Max = 3500;
+        public const int Infinit_Max = 4000;
 
         public static string[] RoleName = { "战士", "法师", "道士" };
         public static string[] PetName = { "战魂", "法魂", "道魂" };
