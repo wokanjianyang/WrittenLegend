a319de4ccee0497c892558072249781bad573f35
wokanjianyang <569284543@qq.com>
2025-05-08 11:02:41 +0800

    t

--- Code/Assets/Scripts/Helper/StringHelper.cs
+++ Code/Assets/Scripts/Helper/StringHelper.cs
@@ -200,7 +200,7 @@ namespace Game
         }
 
         private static string[] UnitList = { "万", "亿", "兆", "京", "垓", "秭", "穰", "沟", "涧", "正", "载", "极", "恒", "河", "沙", "阿", "僧", "祇"
-                , "那", "由", "他", "不", "可", "思" , "议","无","量","大","数‌" ,"古‌" ,"戈" ,"尔‌‌" };
+                , "那", "由", "他", "不", "可", "思" , "议", "无", "量", "大", "数", "古", "戈", "尔" };
 
         private const int Start = 1;
 
@@ -234,188 +234,31 @@ namespace Game
             return src + unit;
         }
 
-        private static string FormatNumberOld(string val, string unit)
-        {
-            string src;
-
-            if (val.Length > 116 + Start)
-            {
-                unit = "数" + unit;
-                src = val.Substring(0, val.Length - 116);
-            }
-            else if (val.Length > 112 + Start)
-            {
-                unit = "大" + unit;
-                src = val.Substring(0, val.Length - 112);
-            }
-            else if (val.Length > 108 + Start)
-            {
-                unit = "量" + unit;
-                src = val.Substring(0, val.Length - 108);
-            }
-            else if (val.Length > 104 + Start)
-            {
-                unit = "无" + unit;
-                src = val.Substring(0, val.Length - 104);
-            }
-            else if (val.Length > 100 + Start)
-            {
-                unit = "议" + unit;
-                src = val.Substring(0, val.Length - 100);
-            }
-            else if (val.Length > 96 + Start)
-            {
-                unit = "思" + unit;
-                src = val.Substring(0, val.Length - 96);
-            }
-            else if (val.Length > 92 + Start)
-            {
-                unit = "可" + unit;
-                src = val.Substring(0, val.Length - 92);
-            }
-            else if (val.Length > 88 + Start)
-            {
-                unit = "不" + unit;
-                src = val.Substring(0, val.Length - 88);
-            }
-            else if (val.Length > 84 + Start)
-            {
-                unit = "他‌" + unit;
-                src = val.Substring(0, val.Length - 84);
-            }
-            else if (val.Length > 80 + Start)
-            {
-                unit = "由" + unit;
-                src = val.Substring(0, val.Length - 80);
-            }
-            else if (val.Length > 76 + Start)
-            {
-                unit = "那" + unit;
-                src = val.Substring(0, val.Length - 76);
-            }
-            else if (val.Length > 72 + Start)
-            {
-                unit = "祇" + unit;
-                src = val.Substring(0, val.Length - 72);
-            }
-            else if (val.Length > 68 + Start)
-            {
-                unit = "僧" + unit;
-                src = val.Substring(0, val.Length - 68);
-            }
-            else if (val.Length > 64 + Start)
-            {
-                unit = "阿" + unit;
-                src = val.Substring(0, val.Length - 64);
-            }
-            else if (val.Length > 60 + Start)
-            {
-                unit = "沙" + unit;
-                src = val.Substring(0, val.Length - 60);
-            }
-            else if (val.Length > 56 + Start)
-            {
-                unit = "河" + unit;
-                src = val.Substring(0, val.Length - 56);
-            }
-            else if (val.Length > 52 + Start)
-            {
-                unit = "恒" + unit;
-                src = val.Substring(0, val.Length - 52);
-            }
-            else if (val.Length > 48 + Start)
-            {
-                unit = "极" + unit;
-                src = val.Substring(0, val.Length - 48);
-            }
-            else if (val.Length > 44)
-            {
-                unit = "载" + unit;
-                src = val.Substring(0, val.Length - 44);
-            }
-            else if (val.Length > 40)
-            {
-                unit = "正" + unit;
-                src = val.Substring(0, val.Length - 40);
-            }
-            else if (val.Length > 36)
-            {
-                unit = "涧" + unit;
-                src = val.Substring(0, val.Length - 36);
-            }
-            else if (val.Length > 32)
-            {
-                unit = "沟" + unit;
-                src = val.Substring(0, val.Length - 32);
-            }
-            else if (val.Length > 28)
-            {
-                unit = "穰" + unit;
-                src = val.Substring(0, val.Length - 28);
-            }
-            else if (val.Length > 24)
-            {
-                unit = "秭" + unit;
-                src = val.Substring(0, val.Length - 24);
-            }
-            else if (val.Length > 20)
-            {
-                unit = "垓" + unit;
-                src = val.Substring(0, val.Length - 20);
-            }
-            else if (val.Length > 16)
-            {
-                unit = "京" + unit;
-                src = val.Substring(0, val.Length - 16);
-            }
-            else if (val.Length > 12)
-            {
-                unit = "兆" + unit;
-                src = val.Substring(0, val.Length - 12);
-            }
-            else if (val.Length > 8)
-            {
-                unit = "亿" + unit;
-                src = val.Substring(0, val.Length - 8);
-            }
-            else if (val.Length > 4)
-            {
-                unit = "万" + unit;
-                src = val.Substring(0, val.Length - 4);
-            }
-            else
-            {
-                return val + unit;
-            }
-
-            if (src.Length < 4)
-            {   //加上点
-                string scale = val.Substring(src.Length, 3 - src.Length).TrimEnd('0');
-                if (scale.Length > 0) //小数位全是0,不显示
-                {
-                    src += "." + scale;
-                }
-                return src + unit;
-            }
-            else
-            {
-                return FormatNumberOld(src, unit);
-            }
-        }
-
         public static double StringToNumber(string text)
         {
-            string unit = "";
+            int e = 0;
+            string res = "";
 
-            for (int i = 0; i < UnitList.Length; i++)
+            for (int i = 0; i < text.Length; i++)
             {
-                unit += "0000";
+                string t = text[i] + "";
+                int index = Array.IndexOf(UnitList, t);
 
-                text = text.Replace(UnitList[i], unit);
+                if (index >= 0)
+                {
+                    e += (index + 1) * 4;
+                }
+                else
+                {
+                    res += t;
+                }
             }
 
-            return Convert.ToDouble(text);
+            res += "E" + e;
+
+            return Convert.ToDouble(res);
         }
+
 
         private static string[] LayerChinaList = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十",
             "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八","十九", "二十" };
