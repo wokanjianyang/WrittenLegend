89a70dd8f4fd3d22669a9158eb2214977139288d
wokanjianyang <569284543@qq.com>
2025-09-03 20:44:28 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -12,11 +12,11 @@ namespace Game
 
         public const int Channel_Tap = 1;
 
-        public const int Version = 404;
+        public const int Version = 405;
 
-        public const long PackTime = 1756048584; //打包时间，防止作弊
+        public const long PackTime = 1756903340; //打包时间，防止作弊
 
-        public const long PackEndTime = 1757776584; //超过此时间,游戏不能使用，需要更新
+        public const long PackEndTime = 1758631340; //超过此时间,游戏不能使用，需要更新
 
         public const long Max_Level = 120000; //最大人物等级和强化等级
 
