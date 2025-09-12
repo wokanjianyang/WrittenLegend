a1d87012cc0b2d045269f03b037e87a9f9d93435
wokanjianyang <569284543@qq.com>
2025-09-12 13:29:17 +0800

    t

--- Code/Assets/Scripts/Skill/Skill_Restore.cs
+++ Code/Assets/Scripts/Skill/Skill_Restore.cs
@@ -31,6 +31,12 @@ namespace Game
                 var teamer = GameProcessor.Inst.PlayerManager.GetPlayer(attackData.Tid);
 
                 var hp = CalcFormula();
+
+                if (teamer.Camp == PlayerType.Defend)
+                {
+                    hp = 10;
+                }
+
                 teamer.OnRestore(attackData.Tid, hp);
 
                 //Debug.Log(this.SelfPlayer.Name + "(" + SelfPlayer.ID + ")" + " Restore to +" + teamer.Name + "(" + teamer.ID + ")" + " :" + hp);
@@ -75,14 +81,14 @@ namespace Game
             foreach (var cell in allAttackCells)
             {
                 var enemy = GameProcessor.Inst.PlayerManager.GetPlayer(cell);
-                if (enemy != null && enemy.HP > 0 && enemy.Camp != PlayerType.Defend && enemy.GroupId == SelfPlayer.GroupId && enemy.ID != SelfPlayer.ID) //只回复同组成员,自己已经加进去了
+                if (enemy != null && enemy.HP > 0 && enemy.GroupId == SelfPlayer.GroupId && enemy.ID != SelfPlayer.ID) //只回复同组成员,自己已经加进去了
                 {
                     teamList.Add(enemy);
                 }
             }
 
             //按损失血量排序
-            teamList = teamList.OrderByDescending(m => m.AttributeBonus.GetAttackDoubleAttr(AttributeEnum.HP) - m.HP).ToList();
+            teamList = teamList.OrderBy(m => m.HP / m.AttributeBonus.GetAttackDoubleAttr(AttributeEnum.HP)).ToList();
 
             foreach (var teamer in teamList)
             {
