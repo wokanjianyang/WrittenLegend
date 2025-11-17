9d95bdfdc1f673365860990d1074740b12fee794
wokanjianyang <569284543@qq.com>
2025-11-17 18:55:53 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -12,7 +12,7 @@ namespace Game
 
         public const int Channel_Tap = 1;
 
-        public const int Version = 415;
+        public const int Version = 416;
 
         public const long PackTime = 1763165461; //打包时间，防止作弊
 
