ccdbe505c76a0fb41024fba63f8697f052f9ba50
wokanjianyang <569284543@qq.com>
2025-11-24 15:47:33 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -12,7 +12,7 @@ namespace Game
 
         public const int Channel_Tap = 1;
 
-        public const int Version = 416;
+        public const int Version = 417;
 
         public const long PackTime = 1763898973; //打包时间，防止作弊
 
--- Code/Assets/Scripts/Skill/SkillGraphic_Jufengpo.cs
+++ Code/Assets/Scripts/Skill/SkillGraphic_Jufengpo.cs
@@ -25,18 +25,32 @@ namespace Game
 
         private IEnumerator IE_Attack(Vector3Int cell)
         {
-            var effectCom = EffectLoader.CreateEffect(this.SkillPanel.SkillData.SkillConfig.ModelName, true, 0, (float)SkillModelConfig.ModelTime);
+            //var effectCom = EffectLoader.CreateEffect(this.SkillPanel.SkillData.SkillConfig.ModelName, true, 0, (float)SkillModelConfig.ModelTime);
 
+            //if (effectCom != null)
+            //{
+            //    var targetPos = GameProcessor.Inst.MapData.GetWorldPosition(cell);
+            //    effectCom.transform.SetParent(GameProcessor.Inst.EffectRoot);
+            //    effectCom.transform.localPosition = targetPos;
+
+            //    yield return new WaitForSeconds((float)SkillModelConfig.ModelTime); //因为现在1s才是一个回合
+            //    GameObject.Destroy(effectCom.gameObject);
+            //}
+            //yield return null;
+
+            var effectCom = EffectLoader.CreateEffect(this.SkillPanel.SkillData.SkillConfig.ModelName, false, 0, (float)SkillModelConfig.ModelTime);
             if (effectCom != null)
             {
+
+                var selfPos = GameProcessor.Inst.MapData.GetWorldPosition(SelfPlayer.Cell);
                 var targetPos = GameProcessor.Inst.MapData.GetWorldPosition(cell);
                 effectCom.transform.SetParent(GameProcessor.Inst.EffectRoot);
-                effectCom.transform.localPosition = targetPos;
+                effectCom.transform.localPosition = selfPos;
+                effectCom.transform.DOLocalMove(targetPos, (float)SkillModelConfig.ModelTime);
 
-                yield return new WaitForSeconds((float)SkillModelConfig.ModelTime); //因为现在1s才是一个回合
+                yield return new WaitForSeconds((float)SkillModelConfig.ModelTime);
                 GameObject.Destroy(effectCom.gameObject);
             }
-            yield return null;
         }
     }
 }
