b1640ec07459ccf8c7a651ad106f69fd362c0aaa
wokanjianyang <569284543@qq.com>
2026-01-25 18:34:22 +0800

    t

--- Code/Assets/Scripts/Window/Dialog_OfflineExp.cs
+++ Code/Assets/Scripts/Window/Dialog_OfflineExp.cs
@@ -52,11 +52,11 @@ namespace Game
             //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Level_Stone, 20000));  //羽毛
             //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Wing_Stone, 100));  //幻境劵
 
-            //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Shuye3, 100)); //专属碎片
-            //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Exclusive_Heart, 100)); //专属之心
-            //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_EquipRefineStone, 1999999999)); //四格碎片
+            //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Reform_Stone, 2000)); //专属碎片
+            //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Pill2, 10000)); //专属之心
+            //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Pill3, 10000)); //四格碎片
 
-            //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_EquipRefineStone, 999999999)); //精炼石
+            //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Talent_Book, 80)); //精炼石
             //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Red_Stone, 999)); //红装精华
             //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Golden_Stone, 999)); //金色精华
             //items.Add(ItemHelper.BuildMaterial(ItemHelper.SpecialId_Dark_Stone, 999)); //暗金精华
