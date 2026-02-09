c9fcc9ab5088a8dd5a3ab25d8b63ba248d618c5f
wokanjianyang <569284543@qq.com>
2026-02-09 15:29:53 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -18,17 +18,17 @@ namespace Game
 
         public const int Channel_Tap = 1;
 
-        public const int Version = 428;
+        public const int Version = 429;
 
-        public const long PackTime = 1770013812; //打包时间，防止作弊
+        public const long PackTime = 1770622014; //打包时间，防止作弊
 
-        public const long PackEndTime = 1771741812; //超过此时间,游戏不能使用，需要更新
+        public const long PackEndTime = 1772782014; //超过此时间,游戏不能使用，需要更新
 
         public const long Max_Level = 120000; //最大人物等级和强化等级
 
         public const long Cycle_Level = 10000; //每次轮回增加等级
 
-        public const long Cycle_Max = 32;
+        public const long Cycle_Max = 35;
 
         public const long Max_Legacy_Level = 20; //最大传世副本等级 
 
