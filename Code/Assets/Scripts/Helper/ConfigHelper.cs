7447cc1557fbc75457c7c8c16fc230ae4566aaf7
wokanjianyang <569284543@qq.com>
2026-08-06 18:17:04 +0800

    t

--- Code/Assets/Scripts/Helper/ConfigHelper.cs
+++ Code/Assets/Scripts/Helper/ConfigHelper.cs
@@ -111,7 +111,7 @@ namespace Game
         public const int PillDefaultTime = 60;
         public const int BabelCount = 300;
         public const int BabelMax = 65000;
-        public const int BabelMythMax = 2000;
+        public const int BabelMythMax = 3000;
         public const int BabelMythCount = 100;
 
         public const int PillMax = 20;
